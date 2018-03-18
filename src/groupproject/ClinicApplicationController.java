eff20ddd02fecd564cad039774c0f5a347b83ca4
JacobFulton <35611763+JacobFulton@users.noreply.github.com>
2018-03-17 20:06:44 -0500

    Add files via upload

---
+++ src/groupproject/ClinicApplicationController.java
@@ -0,0 +1,17 @@
+package groupproject;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class ClinicApplicationController {
+
+    @FXML
+   private Label patientIDV;
+
+    public void initialize(){
+
+
+    }
+
+
+}
